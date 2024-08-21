0250bc471134390143de30e8cee5a2d299448f2e
AkakiEdiSheraShvili0 <81263854+AkakiEdiSheraShvili0@users.noreply.github.com>
2024-08-21 19:56:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+let temperature = 29;
+
+if (temperature < 0) {
+    console.log("ყინავს!");
+} else if (temperature >= 0 && temperature <= 10) {
+    console.log ('ძალიან ცივა')
+} else if (temperature >=11 && temperature <=20) {
+    console.log ("ცივა")
+}else if (temperature >= 21 && temperature <=30) {
+    console.log ("თბილა")
+} else {
+    console.log("ცხელა!")
+}
